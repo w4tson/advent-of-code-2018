bde252b22f2288a751a26b8d842c8bfac45bf1ff
Paul Watson <paul.watson@pdwtech.com>
2018-12-06 21:09:32 +0000

    Day06 part2

--- rust-solutions/src/day06/mod.rs
+++ rust-solutions/src/day06/mod.rs
@@ -1,103 +1,88 @@
 use std::collections::HashMap;
-use crate::utils::dupes;
+use std::str::FromStr;
+use std::fmt::Error;
 
-type Grid = HashMap<(i32, i32), Vec<i32>>;
-type Coord= (i32, i32);
+type Grid = Vec<HashMap<usize, i32>>;
+type Coord = (usize, usize);
 
 struct GridData {
-    grid: HashMap<(i32, i32), Vec<i32>>,
-    size: i32,
-    coords: Vec<(i32, i32)>,
-    ring: i32
+    grid: Grid,
+    size: usize,
+    coords: Vec<Coord>
 }
 
 impl GridData {
 
-    fn size_of_grid(c: &Vec<(i32, i32)>) -> i32 {
+    fn to_coord(line: &str) -> Coord {
+        let mut split = line.split(", ");
+        let x = split.next().map(|m| m.parse().unwrap_or(0)).unwrap();
+        let y = split.next().map(|m| m.parse().unwrap_or(0)).unwrap();
+        (x,y)
+    }
+
+    fn size_of_grid(c: &Vec<Coord>) -> usize {
         c.iter().flat_map(| (x,y)| vec![x,y])
             .max()
             .unwrap() + 1
     }
     
-    pub fn new(coords: Vec<(i32, i32)>) -> GridData {
-        let mut grid = HashMap::new();
+    pub fn new(coords: Vec<Coord>) -> GridData {
+        let size= GridData::size_of_grid(&coords);
+        let mut grid = vec![HashMap::new(); size * size];
         
         coords.iter()
             .enumerate()
             .for_each(|(i,coord)|{
-                grid.insert(*coord, vec![i as i32]);
+                grid[coord.0 + (size * coord.1)].insert(i, 0);
             });
-        GridData { grid, size: GridData::size_of_grid(&coords), coords, ring: 1 }
+        
+        GridData { grid, size, coords }
     }
     
-    pub fn manhatten_dist(&self, coord1: Coord, coord2: Coord) -> i32 {
-        let x = coord1.0 - coord2.0;
-        let y = coord1.1 - coord2.1;
+    pub fn manhatten_dist(&self, coord1: &Coord, coord2: &Coord) -> i32 {
+        let x: i32 = coord1.0 as i32 - coord2.0 as i32;
+        let y: i32 = coord1.1 as i32 - coord2.1 as i32;
         x.abs() + y.abs()
     }
     
-    pub fn round(&mut self) {
-        let foo = &self.coords.to_owned();
-        let new_coords: HashMap<i32, Vec<(i32, i32)>> = foo.iter()
-            .enumerate()
-            .map(|(id, coord)| (id as i32, self.new_growth(coord)))
-            .collect();
+    pub fn calc_area(&mut self, threshold: i32) -> usize {
         
-        let all = new_coords.values()
-            .into_iter()
-            .flat_map(|x| x)
-            .collect();
-        
-        let d : Vec<&(i32, i32)> = dupes(&all);
-        
-        d.iter().for_each(|&x| {
-            println!("{:#?}", x);
-            self.grid.insert( *x, vec![9]);
-        });
-    }
-    
-    fn new_growth(&self, coord: &(i32, i32)) -> Vec<(i32, i32)>{
-        //calculate the ring
-        
-        let width = self.ring * 2 + 1;
-        let mut ring_coords : Vec<(i32 ,i32)> = vec![];
-        
-        for y in coord.1 - self.ring..coord.1 + self.ring+1 {
-            for x in coord.0 - self.ring..coord.0 + self.ring+1 {
-                if x>=0 && x < self.size && y >=0 && y < self.size {
-                    if y == coord.1 - self.ring || y == coord.1 + self.ring {
-                        ring_coords.push((x, y));
-                    } else if x == coord.0 - self.ring || x == coord.0 + self.ring {
-                        ring_coords.push((x, y))
-                    }
-                }
-            }   
+        for y in 0..self.size {
+            for x in 0..self.size {
+                
+                let distances: HashMap<usize, i32> = self.coords
+                    .iter()
+                    .enumerate()
+                    .map(|(id, coord)| (id, self.manhatten_dist(coord, &(x,y))))
+                    .collect();
+                
+                self.grid[x+(y*self.size)] = distances;
+            }
         }
-        ring_coords
-//        println!("{:#?}", ring_coords);
-            
-        //5,5
-        
-        //4,4 5,4 6,4
-        //4,5 5,5 6,5
-        //4,6 5,6 6,6
-        
+
+        self.grid
+            .iter()
+            .map(|distances: &HashMap<usize, i32>| {
+                let sum : i32 = distances.values().sum();
+                sum
+            } )
+            .filter(|total| *total < threshold)
+            .count() 
+
     }
 
-    
-    
     fn print(&self) {
         for y in 0..self.size {
             for x in 0..self.size {
-                let item : String = self.grid.get(&(x,y))
-                    .map(|x| {
-                        if x.len() == 0 { ".".to_string() } else {
-                            let num = x[0];
-                            num.to_string()
-                        }
+                let item : String = self.grid.get(x+(y*self.size))
+                    .map(|distances: &HashMap<usize, i32>| {
+                        distances.iter()
+                            .min_by_key(|(_, &v)| v)
+                            .map(|(k,_ )| format!("{}", k))
+                            .unwrap_or(".".to_string())
                     })
                     .unwrap_or(".".to_string());
-                
+
                 if x == self.size-1 {
                     println!("{}", item);
                 } else {
@@ -109,16 +94,20 @@ impl GridData {
     }
 }
 
-fn solve_part1(input : &str) -> i32 {
-    let coords : Vec<_> = input.lines().map(to_coord).collect();
+impl FromStr for GridData {
+    type Err = Error;
 
-    let mut grid = GridData::new(coords);
-    grid.print();
-    grid.round();
-    grid.print();
-//    grid.grow_area(&(8,9));
+    fn from_str(input: &str) -> Result<Self, <Self as FromStr>::Err> {
+        Ok(GridData::new(input.lines().map(GridData::to_coord).collect()))
+    }
+}
+
+fn solve_part2(input : &str, threshold: i32) -> Result<usize, Error> {
+    let mut grid = GridData::from_str(input)?;
+    let result = grid.calc_area(threshold);
+//    grid.print();
     
-    10
+    Ok(result)
 }
 
 mod tests {
@@ -127,7 +116,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test1() {
+    fn test2() {
         let input = "1, 1
 1, 6
 8, 3
@@ -136,21 +125,13 @@ mod tests {
 8, 9";
         
         println!("{}", input);
-        assert_that!(solve_part1(&input)).is_equal_to(10);
+        assert_that!(solve_part2(&input, 32)).is_equal_to(Ok(16));
     }
 
     #[test]
-    fn part1() {
+    fn part2() {
         let input = read_puzzle_input("day06");
-        let result = solve_part1(&input);
-        println!("{}", result);
+        let result = solve_part2(&input, 10000);
+        println!("{:#?}", result);
     }
 }
-
-fn to_coord(line: &str) -> (i32, i32) {
-    let mut split = line.split(", ");
-    let x = split.next().map(|m| m.parse().unwrap_or(0)).unwrap();
-    let y = split.next().map(|m| m.parse().unwrap_or(0)).unwrap();
-    (x,y)
-}
-
