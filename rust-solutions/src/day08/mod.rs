1462fd1fc3f5c44d4b7519378ecc430c5eaf9f37
Paul Watson <paul.watson@pdwtech.com>
2018-12-08 20:49:53 +0000

    Tidy up

--- rust-solutions/src/day08/mod.rs
+++ rust-solutions/src/day08/mod.rs
@@ -6,16 +6,13 @@ pub mod parse;
 
 #[derive(Debug)]
 pub struct Node {
-    metadata_size: i32,
-    children_size: i32,
-    
     children: Vec<Node>,
     metadata: Vec<i32>
 }
 
 impl Display for Node {
     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        write!(f, "({}, {})\n", self.children_size, self.metadata_size);
+        write!(f, "({}, {})\n", self.children.len(), self.metadata.len());
         for c in &self.children {
             write!(f, "    {}\n", c);
         }
@@ -25,35 +22,25 @@ impl Display for Node {
 
 impl Node {
     pub fn new(children : Vec<Node>, metadata: Vec<i32>) -> Node {
-        Node {
-            metadata_size: metadata.len() as i32, children_size: children.len() as i32, 
-            children, metadata 
-        }
+        Node { children, metadata }
     }
 
-    pub fn new_childless(metadata: Vec<i32>) -> Node {
-        Node {
-            metadata_size: metadata.len() as i32, children_size: 0,
-            children: vec![], metadata
-        }
-    }
-    
     pub fn size(&self) -> usize {
         self.children
             .iter()
             .map(|child| child.size())
-            .sum::<usize>() + self.metadata_size as usize + 2
+            .sum::<usize>() + self.metadata.len() as usize + 2
     }
     
-    pub fn total_metadata(&self) -> i32 {
+    pub fn metadata_sum(&self) -> i32 {
         self.children
             .iter()
-            .map(|child| child.total_metadata())
+            .map(|child| child.metadata_sum())
             .sum::<i32>() + self.metadata.iter().sum::<i32>()
     }
     
     pub fn value(&self) -> i32 {
-        match self.children_size {
+        match self.children.len() {
             0 => self.metadata.iter().sum::<i32>(),
             _ => {
                 self.metadata.iter()
@@ -72,20 +59,23 @@ impl Node {
 
 
 mod tests {
-//    use spectral::assert_that;
+    use spectral::assert_that;
     use super::*;
     use crate::utils::file::read_puzzle_input;
 
     #[test]
-    fn test1() {
+    fn test() {
         let input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2";
-        let nodes : Result<Node, _> = input.parse(); 
-//        assert_that!(solve_part1(&input )).is_equal_to(Ok("CABDFE".to_string()));
+        let node : Node = input.parse().unwrap(); 
+        assert_that!(node.metadata_sum()).is_equal_to(138);
+        assert_that!(node.value()).is_equal_to(66);
     }
 
     #[test]
-    fn part2() {
+    fn part1and2() {
         let input = read_puzzle_input("day08");
-        let nodes : Result<Node, _> = input.parse();
+        let node : Node = input.parse().unwrap();
+        println!("part1 {}", node.metadata_sum());
+        println!("part2 {}", node.value());
     }
 }
--- rust-solutions/src/day08/parse.rs
+++ rust-solutions/src/day08/parse.rs
@@ -3,27 +3,12 @@ use crate::day08::Node;
 use std::fmt::Error;
 use std::prelude::v1::Vec;
 
-// 2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2
-
-
-//
-//
-// 3
-// 2
-
-
-
 pub fn create_node(data: &[i32]) -> Node {
-    
-    let children_size: usize = *data.get(0).unwrap() as usize;
-    let meta : usize = *data.get(1).unwrap() as usize;
+    let (children_size, meta) = (data[0], data[1] as usize);
     let data = &data[2..];
         
     match children_size {
-        0 => {
-            let meta = data[0..meta].to_vec();
-            Node::new_childless(meta)   
-        },
+        0 => Node::new(vec![], data[0..meta].to_vec()),
         _ => { 
             let mut node_size = 0;
             let mut children = vec![];
@@ -40,30 +25,15 @@ pub fn create_node(data: &[i32]) -> Node {
     }
 }
 
-
-
-
 impl FromStr for Node {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, <Self as FromStr>::Err> {
         let data :Vec<i32> = s.to_string()
             .split_whitespace()
-            .map(to_int)
+            .map(|s| s.parse().unwrap_or(0))
             .collect();
-        
-        let tree = create_node(&data);
-        
-        
-        
-        println!("{}", tree.total_metadata());
-        println!("{}", tree.value());
-        println!("{}", tree);
             
-        Ok(Node { metadata_size: 0, children_size:0, children: vec![], metadata: vec![] })
+        Ok(create_node(&data))
     }
-}
-
-fn to_int(s: &str) -> i32 {
-    s.parse().unwrap_or(0)
 }
