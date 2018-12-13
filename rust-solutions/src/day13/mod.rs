4128e2196740927674aac37e9e6e0122ce40218c
Paul Watson <paul.watson@pdwtech.com>
2018-12-13 14:08:41 +0000

    day 13

--- rust-solutions/src/day13/mod.rs
+++ rust-solutions/src/day13/mod.rs
@@ -5,6 +5,7 @@ use lazy_static::lazy_static;
 use regex::Regex;
 use itertools::Itertools;
 use std::prelude::v1::Vec;
+use std::collections::HashMap;
 
 pub mod parse;
 
@@ -34,16 +35,83 @@ enum Direction {
     West
 }
 
+fn mode(numbers: &Vec<(usize, usize)>) -> ((usize, usize), usize) {
+    let mut occurrences = HashMap::new();
 
+    for &value in numbers {
+        *occurrences.entry(value).or_insert(0) += 1;
+    }
+
+    occurrences
+        .into_iter()
+        .max_by_key(|&(_, count)| count)
+        .expect("Cannot compute the mode of zero numbers")
+}
 
 struct Cart {
-    location : (u32, u32),
-    facing: Direction
+    location : (usize, usize),
+    facing: Direction,
+    intersections: usize
 }
 
 impl Cart {
-    pub fn new(facing: Direction, location: (u32, u32)) -> Cart {
-        Cart { location, facing }
+    pub fn new(facing: Direction, location: (usize, usize)) -> Cart {
+        Cart { location, facing, intersections: 0 }
+    }
+
+    pub fn to_char(&self) -> char {
+        match self.facing {
+            Direction::North => '^',
+            Direction::South => 'v',
+            Direction::West => '<',
+            Direction::East => '>',
+            _ => '!'
+        }
+    }
+
+    pub fn advance_north(&mut self) {
+        self.location.1 -= 1;
+    }
+
+    pub fn advance_south(&mut self) {
+        self.location.1 += 1;
+    }
+
+    pub fn advance_east(&mut self) {
+        self.location.0 += 1;
+    }
+
+    pub fn advance_west(&mut self) {
+        self.location.0 -= 1;
+    }
+
+    pub fn advance(&mut self) {
+        match &self.facing {
+            Direction::North =>  self.location.1 -= 1,
+            Direction::South =>  self.location.1 += 1,
+            Direction::East =>  self.location.0 += 1,
+            Direction::West =>  self.location.0 -= 1
+        }
+    }
+
+    pub fn turn_clockwise(&mut self) {
+//        println!("turn clockwise");
+        match self.facing {
+            Direction::North => self.facing = Direction::East,
+            Direction::South => self.facing = Direction::West,
+            Direction::East => self.facing = Direction::South,
+            Direction::West => self.facing = Direction::North
+        }
+    }
+
+    pub fn turn_counter_clockwise(&mut self) {
+//        println!("turn counter clockwise");
+        match self.facing {
+            Direction::North => self.facing = Direction::West,
+            Direction::East => self.facing = Direction::North,
+            Direction::South => self.facing = Direction::East,
+            Direction::West => self.facing = Direction::South
+        }
     }
 
 }
@@ -53,12 +121,11 @@ impl FromStr for Track {
 
     fn from_str(s: &str) -> Result<Self> {
         let width = s.lines().map(|l| l.len()).max().unwrap();
+        println!("width {}", width);
         let mut carts = vec![];
         let track : Vec<Vec<TrackType>> = s.lines().enumerate()
             .map(|(y,line)|{
-                let y = y as u32;
                 let mut row : Vec<TrackType> = line.chars().enumerate().map(|(x, ch)|{
-                    let x = x as u32;
                     match ch {
                         '|' => TrackType::Vertical,
                         '-' => TrackType::Horizontal,
@@ -73,7 +140,8 @@ impl FromStr for Track {
                         _ => panic!("{}", ch)
                 }}).collect();
                 let len = row.len();
-                for n in len..=(width-len) {
+
+                for n in 0..(width-len) {
                     row.push(TrackType::None);
                 }
                 row
@@ -94,9 +162,88 @@ impl FromStr for Track {
 }
 
 impl Track {
-  pub fn move_carts() {
-      
+    pub fn print_it(&mut self) {
+        for y in 0..self.track.len() {
+            let row = &self.track[y];
+            for x in 0..row.len() {
+                let ch = match self.track[y][x] {
+                    TrackType::Horizontal => '-',
+                    TrackType::Vertical => '|',
+                    TrackType::Left => '\\',
+                    TrackType::Right => '/',
+                    TrackType::Intersection => '+',
+                    TrackType::None => '.',
+                    _ => '.'
+                };
+
+                let ch = self.carts.iter().find(|cart| cart.location == (x,y))
+                    .map(|cart| cart.to_char())
+                    .unwrap_or_else(|| ch);
+                print!("{}", ch);
+            }
+            println!();
+        }
+    }
+
+  pub fn move_carts(&mut self) -> Option<(usize, usize)> {
+     self.carts.sort_by_key(|cart| cart.location);
+     for mut cart in &mut self.carts {
+         
+         let (x,y) = cart.location;
+         let track = &self.track[y][x];
+         
+
+         //     |
+         //---  /---
+//              |
+         match (track, &cart.facing) {
+             (TrackType::Left, Direction::North) | (TrackType::Left, Direction::South) => cart.turn_counter_clockwise(),
+             (TrackType::Left, Direction::East) | (TrackType::Left, Direction::West)=> cart.turn_clockwise(),
+             (TrackType::Right, Direction::North) | (TrackType::Right, Direction::South) => cart.turn_clockwise(),
+             (TrackType::Right, Direction::East) | (TrackType::Right, Direction::West)=> cart.turn_counter_clockwise(),
+             (TrackType::Intersection,_) => {
+                match cart.intersections % 3 {
+                    0 => cart.turn_counter_clockwise(),
+                    1 => { },
+                    2 => cart.turn_clockwise(),
+                    _ => panic!("doh")
+                }
+                cart.intersections += 1;
+             },
+             _ => {
+//                 println!("no op");
+                 //no op
+             }
+         }
+
+         cart.advance();
+
+
+
+
+     }
+
+      let deduped_total = &self.carts.iter()
+          .map(|cart| cart.location)
+          .dedup()
+          .count();
+
+      let ((x,y), count) = mode(&self.carts.iter().map(|c| c.location).collect());
+//      if self.carts.len() != *deduped_total {
+//          println!("CRASH!!!! ");
+//          self.carts.iter().for_each(|c| {
+//              println!("{},{}", c.location.0, c.location.1);
+//          })
+//
+//      }
+//     self.print_it();
+      match count > 1 {
+          true => Some((x,y)),
+          _ => None
+      }
   }
+
+
 
 }
 
@@ -110,16 +257,34 @@ mod tests {
     #[test]
     fn test() {
         let input = include_str!("test_data");
-        let track : Track = input.parse()
+        let mut track : Track = input.parse()
                         .unwrap_or_else(|_| panic!("Couldn't parse {}", input));
-       
-            
+
+        track.print_it();
+        for i in 0..15 {
+            println!("i = {}", i);
+            track.move_carts();
+        }
 //        assert_that!(foos[1].bar()).is_equal_to(2);
+
+
     }
 
     #[test]
     fn part1() {
-//        let input = read_puzzle_input("day11");
+        let input = read_puzzle_input("day13");
+        let mut track : Track = input.parse()
+            .unwrap_or_else(|_| panic!("Couldn't parse {}", input));
+
+        track.print_it();
+        let mut found = false;
+
+        while !found {
+            match track.move_carts() {
+                Some(crashed) => { println!("{:#?}" , crashed); break; },
+                _ => {}
+            }
+        }
 //        let foo : Foo = input.parse().unwrap();
 //        println!("part1 {}", node.bar());
     }
