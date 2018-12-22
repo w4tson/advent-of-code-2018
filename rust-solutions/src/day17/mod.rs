107278b54d796c8599bda15b8d28433f561c4e1b
Paul Watson <paul.watson@pdwtech.com>
2018-12-22 19:49:56 +0000

    Day17

--- rust-solutions/src/day17/mod.rs
+++ rust-solutions/src/day17/mod.rs
@@ -1,18 +1,224 @@
 pub mod parse;
 use crate::day17::parse::*;
+use std::ops::Range;
+use std::fmt::Display;
+use std::collections::VecDeque;
+use std::collections::HashMap;
+use std::option::Option::Some;
 
-pub struct Slice {
-    map : Vec<Vec<char>>
+const WATER : char = '|';
+const EMPTY : char = '.';
+const WALL  : char = '#';
+const STILL_WATER  : char = '~';
+
+pub type Coord = (usize, usize);
+pub struct CrossSection {
+    map : Vec<Vec<char>>,
+    width: FromTo,
+    height: FromTo,
+    stream: VecDeque<Coord>,
+    seen: HashMap<Coord, usize>
 }
+
+#[derive(Copy, Clone)]
+pub struct FromTo {
+    start: usize,
+    end: usize,
+}
+
+pub enum Flow {
+    Left,
+    Right,
+    LeftAndRight
+}
+
+
+impl CrossSection {
+    pub fn add_water(&mut self, x: usize, y: usize) {
+        self.map[y][x] = WATER;
+        self.stream.push_back((x, y));
+    }
+
+    pub fn is_water(&self, c: Coord) -> bool {
+        self.map[c.1][c.0] == WATER
+    }
+
+    pub fn char_at_rel(&self, x: usize, y: usize) -> char {
+        self.map[y][x - self.width.start]
+    }
+
+    fn update(&mut self, c: Coord, ch: char) {
+        self.map[c.1][c.0] = ch;
+    }
+
+    pub fn is_container(&self, c: Coord) -> bool {
+        let ch = self.map[c.1][c.0];
+        ch == WALL || ch == STILL_WATER
+    }
+
+    pub fn is_contained_down(&self, c: Coord) -> bool {
+        c.1 < self.height.count() - 1 && self.is_container((c.0, c.1 + 1))
+    }
+
+    pub fn is_contained_left(&self, c: Coord) -> bool {
+        let left = (c.0 - 1, c.1);
+        c.0 > 0 && (self.is_container(left) || (self.is_water(left) && self.is_contained_left(left)))
+    }
+
+    pub fn is_contained_right(&self, c: Coord) -> bool {
+        let right = (c.0 + 1, c.1);
+        c.0 < self.width.count() - 1 && (self.is_container(right) || (self.is_water(right) && self.is_contained_right(right)))
+    }
+
+    fn is_contained(&self, c: Coord) -> bool {
+        self.is_contained_down(c) && self.is_contained_left(c) && self.is_contained_right(c)
+    }
+
+    pub fn can_flow_down(&self, c: Coord) -> Option<Coord> {
+        if c.1 < self.height.count() - 1 && self.map[c.1 + 1][c.0] == EMPTY {
+            Some((c.0, c.1 + 1))
+        } else {
+            None
+        }
+    }
+
+    pub fn can_flow_left(&self, c: Coord) -> Option<Coord> {
+        if c.0 > 0 && self.map[c.1][c.0 - 1] == EMPTY
+            && self.is_contained_down(c)
+            && c.1 != self.height.count() - 1 {
+            Some((c.0 - 1, c.1))
+        } else {
+            None
+        }
+    }
+
+    pub fn can_flow_right(&self, c: Coord) -> Option<Coord> {
+        if c.0 < self.width.count() - 1 &&
+            self.map[c.1][c.0 + 1] == EMPTY
+            && self.is_contained_down(c)
+            && c.1 != self.height.count() - 1 {
+            Some((c.0 + 1, c.1))
+        } else {
+            None
+        }
+    }
+
+    pub fn can_flow(&self, c: Coord) -> bool {
+        self.can_flow_right(c).is_some() || self.can_flow_left(c).is_some() || self.can_flow_down(c).is_some()
+    }
+
+    fn flow(&mut self, coord: Coord) -> Vec<Coord> {
+        let mut new_droplets = vec![];
+        if let Some(down) = self.can_flow_down(coord) {
+            &self.seen.entry(down).or_insert_with(|| {
+                new_droplets.push(down);
+                1
+            });
+//                new_droplets.push(down);
+            self.update(down, WATER);
+        } else {
+            if let Some(left) = self.can_flow_left(coord) {
+                self.seen.entry(left).or_insert_with(|| {
+                    new_droplets.push(left);
+                    1
+                });
+//                    new_droplets.push(left);
+                self.update(left, WATER);
+            }
+
+            if let Some(right) = self.can_flow_right(coord) {
+                &self.seen.entry(right).or_insert_with(|| {
+                    new_droplets.push(right);
+                    1
+                });
+//                    new_droplets.push(right);
+                self.update(right, WATER);
+            }
+        }
+        new_droplets
+    }
+
+    pub fn stream(&mut self) {
+        for i in 0.. {
+            let mut new_droplets: Vec<Coord> = vec![];
+            let mut flowed = false;
+
+            for s in 0..self.stream.len() {
+                //can it move
+                //if it is occupied
+                let coord = self.stream[s];
+                if self.is_water(coord) {
+                    if self.can_flow(coord) {
+                        let new_drops = self.flow(coord);
+                        new_droplets.extend(new_drops.iter());
+                        flowed = true;
+                    } else {
+                        //turn water still
+                        if self.is_contained(coord) {
+                            flowed = true;
+                            //turn this to still water
+                            self.update(coord, STILL_WATER);
+                        }
+                    }
+                }
+            }
+
+
+            if self.char_at_rel(500, 0) == EMPTY {
+                self.add_water(500 - self.width.start, 0);
+            } else if !flowed {
+                println!("still {}", self.count_still_water());
+                break;
+            }
+            self.stream.extend(new_droplets.iter());
+//            println!("\n{}\nRound {}", &self, i);
+        }
+    }
+
+    pub fn count_still_water(&self) -> usize {
+        self.map.iter().map(|row| {
+            row.iter().filter(|&ch| *ch == STILL_WATER).count()
+        }).sum()
+    }
+
+    pub fn count_water(&self) -> usize {
+        self.map.iter().map(|row| {
+            row.iter().filter(|&ch| *ch == WATER || *ch == STILL_WATER).count()
+        }).sum()
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::fmt::Error;
+    use crate::day17::parse::*;
+    use crate::utils::file::read_puzzle_input;
 
     #[test]
     fn test() {
         let input = include_str!("test_data");
-        let slice : Result<Slice, Error> = input.parse();
+        if let Ok(cross_section) = input.parse::<CrossSection>() {
+            let mut cross_section = cross_section;
+            println!("{}", cross_section);
+            cross_section.stream();
+            let count = cross_section.count_water();
+            eprintln!("count = {:#?}", count);
+            
+        }
+    }
+
+    #[test]
+    fn part1() {
+        let input = read_puzzle_input("day17");
+        if let Ok(cross_section) = input.parse::<CrossSection>() {
+            let mut cross_section = cross_section;
+//            println!("{}", cross_section);
+            cross_section.stream();
+            let count = cross_section.count_water();
+            eprintln!("count = {:#?}", count);
+
+        }
     }
 }
--- rust-solutions/src/day17/parse.rs
+++ rust-solutions/src/day17/parse.rs
@@ -1,8 +1,14 @@
 use std::str::FromStr;
-use crate::day17::Slice;
+use crate::day17::CrossSection;
 use std::fmt::Error;
 use std::ops::Range;
 use itertools::Itertools;
+use std::fmt::Display;
+use std::fmt::Formatter;
+use crate::day17::FromTo;
+use std::collections::VecDeque;
+use std::collections::HashSet;
+use std::collections::HashMap;
 
 //x=495, y=2..7
 //y=7, x=495..501
@@ -13,18 +19,57 @@ use itertools::Itertools;
 //x=504, y=10..13
 //y=13, x=498..504
 
-struct ClayArea {
-    x: Range<usize>,
-    y: Range<usize>
+
+impl FromTo {
+    pub fn range(&self) -> Range<usize> {
+        Range{ start: self.start, end: self.end+1 }
+    }
+    
+    pub fn count(&self) -> usize {
+        self.end + 1 - self.start
+    }
 }
 
-impl FromStr for Slice {
+struct ClayArea {
+    x: FromTo,
+    y: FromTo
+}
+
+impl FromStr for CrossSection {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, <Self as FromStr>::Err> {
-        s.lines().map(|line| to_clay_area(line)).collect_vec();
+        let areas = s.lines().map(|line| to_clay_area(line)).collect_vec();
         
-        Ok(Slice{ map: vec![] })
+        let minx = areas.iter().min_by_key(|area| area.x.start).unwrap().x.start -1;
+        let maxx = areas.iter().max_by_key(|area| area.x.end).unwrap().x.end +1;
+        let miny = areas.iter().min_by_key(|area| area.y.start).unwrap().y.start;
+        let maxy = areas.iter().max_by_key(|area| area.y.end).unwrap().y.end;
+
+        let width = FromTo{ start: minx, end: maxx  };
+        let height = FromTo{ start: miny, end: maxy  };
+
+        let w = width.count();
+        let h = height.count();
+        let map = vec![vec!['.'; w]; h];
+        
+        
+        println!("{}..{} {}",minx, maxx, w);
+        println!("{}..{} {}",miny, maxy, h);
+        
+        let mut map = areas.iter().fold(map, | mut m, area| {
+            for y in area.y.range() {
+                for x in area.x.range() {
+                    m[y-height.start][x-width.start] = '#';
+                }
+            }
+            m
+        });
+        
+//        map[0][500-width.start] = '+';
+        
+        
+        Ok(CrossSection { map, width, height, stream: VecDeque::new(), seen: HashMap::new() })
     }
 }
 
@@ -53,16 +98,29 @@ fn to_clay_area(line: &str) -> ClayArea {
     }
 }
 
-fn to_range(s: &str) -> Range<usize> {
+fn to_range(s: &str) -> FromTo {
     if s.contains("..") {
         let mut from_to = s.split("..");
         let from : usize = from_to.next().unwrap().parse().unwrap_or_else(|_| panic!("Couldn't parse from {}", s));
         let to : usize = from_to.next().unwrap().parse().unwrap_or_else(|_| panic!("Couldn't parse to   {}", s));
-        Range { start: from, end: to + 1 }
+        FromTo { start: from, end: to  }
 
     } else {
         let num: usize = s.parse().unwrap_or_else(|_| panic!("Couldn't parse single reuslt  {}", s));
-        Range { start: num, end: num +1 }
+        FromTo { start: num, end: num }
+    }
+}
+
+impl Display for CrossSection {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), std::fmt::Error> {
+        for y in 0..self.height.count() {
+            for x in 0..self.width.count() {
+                write!(f, "{}", self.map[y][x]);
+            }
+            writeln!(f);
+        }
+        
+        write!(f,"")
     }
 }
 
